8d987f39743e4c92a0823b7fe67b21d3f08a0a80
ngvadait <ngvadait@gmail.com>
2019-01-04 22:24:12 +0700

    Api get info movie

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get('/movies', (req, res) => {
     return res.send(movieStore.all());
 });
 
+app.get('/movies/:title', (req, res) => {
+    let foundMovies = movieStore.find(req.params.title);
+
+    if (foundMovies.length < 1) {
+        res.statusCode = 404;
+        return res.send({
+            message: 'movie not found'
+        })
+    }
+
+    return res.send({
+        message: 'found movie',
+        payload: foundMovies.pop()
+    });
+});
+
 app.listen(3000, () => {
     console.log('server started at: 127.0.0.1:3000');
 });
